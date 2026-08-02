138a683dd3994ac3b06fac42d51c095027326cae
visalkhp <51016030+visalkhp@users.noreply.github.com>
2026-08-02 22:41:51 +0530

    Delete js/pwa.js

--- js/pwa.js
+++
@@ -1,5 +0,0 @@
-export async function registerServiceWorker(){
- if("serviceWorker" in navigator){await navigator.serviceWorker.register("./sw.js");}
-}
-export function initializeInstallPrompt(){}
-export function watchInstallation(){}
